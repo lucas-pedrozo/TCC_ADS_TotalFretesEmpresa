e960dcb66e342883c542f6abea96124183a8bf5f
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2026-04-12 18:49:35 -0300

    feat(init): init

---
+++ src/components/custom/buttons/ButtonDefault.tsx
@@ -0,0 +1,30 @@
+type ButtonDefaultProps = {
+  dataTestid?: string
+  disabled?: boolean
+  onClick?: () => void
+  name?: string
+  className?: string
+  type?: "default" | "primary" | "secondary" | "danger"
+}
+
+const BUTTON_STYLES = {
+  default: "bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded",
+  primary: "bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded",
+  secondary: "bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded",
+  danger: "bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded",
+} as const
+
+export const ButtonDefault = (props: ButtonDefaultProps) => {
+  return (
+    <div className={props.className}>
+      <button
+        data-testid={props.dataTestid}
+        disabled={props.disabled}
+        onClick={props.onClick}
+        className={props.disabled ? "bg-gray-400 text-white font-bold py-2 px-4 rounded cursor-not-allowed" : BUTTON_STYLES[props.type || "default"]}
+      >
+        {props.name}
+      </button>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Router } from './routes/Routes'
+import { BrowserRouter } from 'react-router';
+import App from './routes/App'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Router />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root")!;
+
+createRoot(rootElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
